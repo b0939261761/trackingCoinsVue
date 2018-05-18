ea10d902657d8609346236f639f034549ebbf434
Bogdan <bogdan_uman@meta.ua>
2018-05-18 17:04:35 +0300

    fix

--- src/views/Exchanges.vue
+++ src/views/Exchanges.vue
@@ -1,23 +1,20 @@
 <template lang="pug">
-  v-card( height='100%' class="layout-mb" )
-    v-card-text
-      v-container( grid-list-lg )
-        v-layout
-          v-flex(
-            xs12
-            sm12
-            md12
-          )
-           v-data-table.elevation-1(:headers='headers', :items='desserts')
-            template(slot='items', slot-scope='props')
-              td.text-xs-left {{ props.item.name }}
-              td.text-xs-left {{ props.item.logotype }}
-              td.text-xs-left {{ props.item.curr }}
-              td.text-xs-left {{ props.item.btc_quantity }}
-              td.text-xs-left {{ props.item.quantity }}
-              td.text-xs-left {{ props.item.supp_lang }}
-              td.text-xs-left {{ props.item.year }}
-
+  v-card
+    v-card-title
+      | Nutrition
+      v-spacer
+      v-text-field(v-model='search', append-icon='search', label='Search', single-line='', hide-details='')
+    v-data-table.elevation-1(:headers='headers', :items='desserts', :search='search')
+      template(slot='items', slot-scope='props')
+        td.text-xs-left {{ props.item.name }}
+        td.text-xs-left {{ props.item.logotype }}
+        td.text-xs-left {{ props.item.curr }}
+        td.text-xs-left {{ props.item.btc_quantity }}
+        td.text-xs-left {{ props.item.quantity }}
+        td.text-xs-left {{ props.item.supp_lang }}
+        td.text-xs-left {{ props.item.year }}
+      v-alert(slot='no-results', :value='true', color='error', icon='warning')
+        | Your search for "{{ search }}" found no results.
 </template>
 
 <style>
@@ -30,67 +27,67 @@
 </style>
 
 <script>
-  export default {
-    data () {
-      return {
-        title: 'Заголовок',
-        headers: [
-          {
-            text: 'п.п',
-            align: 'left',
-            sortable: true,
-            value: 'name'
-          },
-          { text: 'Логотип', value: 'logotype' },
-          { text: 'Биржа', value: 'curr' },
-          { text: 'Объем торгов', value: 'btc_quantity' },
-          { text: 'К-во пар', value: 'quantity' },
-          { text: 'Поддержка языков', value: 'supp_lang' },
-          { text: 'Год основания', value: 'year' }
-        ],
-        desserts: [
-          {
-            value: false,
-            name: '1',
-            logotype: 'Логотип',
-            curr: 'POLONEIX',
-            btc_quantity: 2544.57,
-            quantity: 12,
-            supp_lang: 'ENG RUS',
-            year: '2010'
-          },
-          {
-            value: false,
-            name: '2',
-            logotype: 'Логотип',
-            curr: 'Bitfic',
-            btc_quantity: 2544.57,
-            quantity: 12,
-            supp_lang: 'ENG RUS',
-            year: '2010'
-          },
-          {
-            value: false,
-            name: '3',
-            logotype: 'Логотип',
-            curr: 'YouBit',
-            btc_quantity: 2544.57,
-            quantity: 12,
-            supp_lang: 'ENG RUS',
-            year: '2010'
-          },
-          {
-            value: false,
-            name: '4',
-            logotype: 'Логотип',
-            curr: 'Kraken',
-            btc_quantity: 2544.57,
-            quantity: 12,
-            supp_lang: 'ENG RUS',
-            year: '2010'
-          },
-        ]
-      }
+export default {
+  data () {
+    return {
+      search: '',
+      headers: [
+        {
+          text: 'п.п',
+          align: 'left',
+          sortable: true,
+          value: 'name'
+        },
+        { text: 'Логотип', value: 'logotype' },
+        { text: 'Биржа', value: 'curr' },
+        { text: 'Объем торгов', value: 'btc_quantity' },
+        { text: 'К-во пар', value: 'quantity' },
+        { text: 'Поддержка языков', value: 'supp_lang' },
+        { text: 'Год основания', value: 'year' }
+      ],
+      desserts: [
+        {
+          value: false,
+          name: '1',
+          logotype: 'Логотип',
+          curr: 'POLONEIX',
+          btc_quantity: 2544.57,
+          quantity: 12,
+          supp_lang: 'ENG RUS',
+          year: '2010'
+        },
+        {
+          value: false,
+          name: '2',
+          logotype: 'Логотип',
+          curr: 'Bitfic',
+          btc_quantity: 2544.57,
+          quantity: 12,
+          supp_lang: 'ENG RUS',
+          year: '2010'
+        },
+        {
+          value: false,
+          name: '3',
+          logotype: 'Логотип',
+          curr: 'YouBit',
+          btc_quantity: 2544.57,
+          quantity: 12,
+          supp_lang: 'ENG RUS',
+          year: '2010'
+        },
+        {
+          value: false,
+          name: '4',
+          logotype: 'Логотип',
+          curr: 'Kraken',
+          btc_quantity: 2544.57,
+          quantity: 12,
+          supp_lang: 'ENG RUS',
+          year: '2010'
+        },
+      ]
     }
   }
+}
 </script>
--- src/views/Pairs.vue
+++ src/views/Pairs.vue
@@ -1,22 +1,20 @@
 <template lang="pug">
-  v-card( height='100%' class="layout-mb" )
-    v-card-text
-      v-container( grid-list-lg )
-        v-layout
-          v-flex(
-            xs12
-            sm12
-            md12
-          )
-           v-data-table.elevation-1(:headers='headers', :items='desserts')
-            template(slot='items', slot-scope='props')
-              td.text-xs-left {{ props.item.name }}
-              td.text-xs-left {{ props.item.para }}
-              td.text-xs-left {{ props.item.last_price }}
-              td.text-xs-left {{ props.item.change_price }}
-              td.text-xs-left {{ props.item.amount }}
-              td.text-xs-left {{ props.item.high }}
-              td.text-xs-left {{ props.item.low }}
+  v-card
+    v-card-title
+      | Nutrition
+      v-spacer
+      v-text-field(v-model='search', append-icon='search', label='Search', single-line='', hide-details='')
+    v-data-table(:headers='headers', :items='desserts', :search='search')
+      template(slot='items', slot-scope='props')
+        td.text-xs-left {{ props.item.name }}
+        td.text-xs-left {{ props.item.para }}
+        td.text-xs-left {{ props.item.last_price }}
+        td.text-xs-left {{ props.item.change_price }}
+        td.text-xs-left {{ props.item.amount }}
+        td.text-xs-left {{ props.item.high }}
+        td.text-xs-left {{ props.item.low }}
+      v-alert(slot='no-results', :value='true', color='error', icon='warning')
+        | Your search for "{{ search }}" found no results.
 </template>
 
 <style>
@@ -29,67 +27,67 @@
 </style>
 
 <script>
-  export default {
-    data () {
-      return {
-        title: 'Заголовок',
-        headers: [
-          {
-            text: 'п.п',
-            align: 'left',
-            sortable: true,
-            value: 'name'
-          },
-          { text: 'Пара', value: 'para' },
-          { text: 'Последняя цена', value: 'last_price' },
-          { text: 'Изминение цены', value: 'change_price' },
-          { text: 'Суточный объем', value: 'amount' },
-          { text: 'High', value: 'high' },
-          { text: 'Low', value: 'low' }
-        ],
-        desserts: [
-          {
-            value: false,
-            name: '1',
-            para: 'BTC_USD',
-            last_price: 25442.57,
-            change_price: 3.5,
-            amount: 8860.56,
-            high: '0.0265',
-            low: '0.0154'
-          },
-          {
-            value: false,
-            name: '2',
-            para: 'BTC_USD',
-            last_price: 25442.57,
-            change_price: 3.5,
-            amount: 8860.56,
-            high: '0.0265',
-            low: '0.0154'
-          },         
-          {
-            value: false,
-            name: '3',
-            para: 'BTC_USD',
-            last_price: 25442.57,
-            change_price: 3.5,
-            amount: 8860.56,
-            high: '0.0265',
-            low: '0.0154'
-          },
-          {
-            value: false,
-            name: '4',
-            para: 'BTC_USD',
-            last_price: 25442.57,
-            change_price: 3.5,
-            amount: 8860.56,
-            high: '0.0265',
-            low: '0.0154'
-          },                   
-        ]
-      }
+export default {
+  data () {
+    return {
+      search: '',
+      headers: [
+        {
+          text: 'п.п',
+          align: 'left',
+          sortable: true,
+          value: 'name'
+        },
+        { text: 'Пара', value: 'para' },
+        { text: 'Последняя цена', value: 'last_price' },
+        { text: 'Изминение цены', value: 'change_price' },
+        { text: 'Суточный объем', value: 'amount' },
+        { text: 'High', value: 'high' },
+        { text: 'Low', value: 'low' }
+      ],
+      desserts: [
+        {
+          value: false,
+          name: '1',
+          para: 'BTC_USD',
+          last_price: 25442.57,
+          change_price: 3.5,
+          amount: 8860.56,
+          high: '0.0265',
+          low: '0.0154'
+        },
+        {
+          value: false,
+          name: '2',
+          para: 'BTC_USD',
+          last_price: 25442.57,
+          change_price: 3.5,
+          amount: 8860.56,
+          high: '0.0265',
+          low: '0.0154'
+        },
+        {
+          value: false,
+          name: '3',
+          para: 'BTC_USD',
+          last_price: 25442.57,
+          change_price: 3.5,
+          amount: 8860.56,
+          high: '0.0265',
+          low: '0.0154'
+        },
+        {
+          value: false,
+          name: '4',
+          para: 'BTC_USD',
+          last_price: 25442.57,
+          change_price: 3.5,
+          amount: 8860.56,
+          high: '0.0265',
+          low: '0.0154'
+        },
+      ]
     }
   }
+}
 </script>
